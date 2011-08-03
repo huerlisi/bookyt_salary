d27b96dadb4616cc8d1652504eee0d21209f73d1
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-08-03 15:11:09 +0200

    Use app.config.bookyt.engines to register engines with bookyt.

--- lib/bookyt_pos/railtie.rb
+++ lib/bookyt_pos/railtie.rb
@@ -3,8 +3,8 @@ require 'rails'
 
 module BookytPos
   class Railtie < Rails::Engine
-    initializer :after_initialize do
-      Bookyt::Engine.register_engine 'bookyt_pos'
+    initializer :after_initialize do |app|
+      app.config.bookyt.engines << 'bookyt_pos'
     end
   end
 end
