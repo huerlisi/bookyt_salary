47a3b4455f25b165bb583128bb5992c6a95becf9
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-11-10 11:39:57 +0100

    Move salaries routes from bookyt.

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,12 @@
 Rails.application.routes.draw do
+  # Salaries
+  resources :salaries do
+    collection do
+      get :statistics
+    end
+
+    member do
+      get :payslip
+    end
+  end
 end
