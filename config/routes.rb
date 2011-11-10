570508c0600799a0d36074ce531f852b1037f56d
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-11-10 14:28:43 +0100

    Include line items logic for salaries.

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,13 @@ Rails.application.routes.draw do
     member do
       get :payslip
     end
+
+    # Line Items
+    member do
+      get :new_line_item
+    end
+    collection do
+      get :new_line_item
+    end
   end
 end
