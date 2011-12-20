09aef897d797d69bba846843ba83dd6f6603cae1
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-12-20 22:08:23 +0100

    Fix application config in spec dummy.

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -7,7 +7,7 @@ require "action_view/railtie"
 require "action_mailer/railtie"
 
 Bundler.require
-require "bookyt_pos"
+require "bookyt_salary"
 
 module Dummy
   class Application < Rails::Application
