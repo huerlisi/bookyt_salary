b8538a8f101c346d74e8cb2e0d1d4615eaecd10c
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-12-21 10:16:51 +0100

    Fix new salary navigational action.

--- lib/bookyt_salary/navigation.rb
+++ lib/bookyt_salary/navigation.rb
@@ -4,7 +4,7 @@ module BookytSalary
       navigation.item :salaries, t_title(:index, Salary), salaries_path,
                    :if => Proc.new { user_signed_in? } do |salaries|
         salaries.item :salaries, t_title(:index, Salary), salaries_path, :highlights_on => /\/salaries($|\/[0-9]*($|\/.*))/
-        salaries.item :new_salary, t_title(:new, Salary), new_salary_path
+        salaries.item :new_salary, t_title(:new, Salary), select_employee_salaries_path
         salaries.item :employees, t_title(:index, Employee), employees_path
         salaries.item :employments, t_title(:index, Employment), employments_path, :highlights_on => /\/employments($|\/[0-9]*($|\/.*))/
         salaries.item :salary_statistics, t_title(:statistics, Salary), statistics_salaries_path, :highlights_on => /\/salaries\/statistics($|\?)/
