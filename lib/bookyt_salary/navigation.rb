d1e6aae0a1b3392bfd87c5be2e0351d1e37372a9
Roman Simecek <roman.simecek@cyt.ch>
2012-01-13 13:20:13 +0100

    I dont know.

--- lib/bookyt_salary/navigation.rb
+++ lib/bookyt_salary/navigation.rb
@@ -1,9 +1,9 @@
 module BookytSalary
   module Navigation
     def setup_bookyt_salary(navigation)
-      navigation.item :salaries, t_title(:index, Salary), salaries_path,
+      navigation.item :salaries, t('bookyt.main_navigation.salaries'), salaries_path,
                    :if => Proc.new { user_signed_in? } do |salaries|
-        salaries.item :salaries, t_title(:index, Salary), salaries_path, :highlights_on => /\/salaries($|\/[0-9]*($|\/.*))/
+        salaries.item :salaries, t('bookyt.main_navigation.salaries'), salaries_path, :highlights_on => /\/salaries($|\/[0-9]*($|\/.*))/
         salaries.item :new_salary, t_title(:new, Salary), select_employee_salaries_path
         salaries.item :employees, t_title(:index, Employee), employees_path
         salaries.item :employments, t_title(:index, Employment), employments_path, :highlights_on => /\/employments($|\/[0-9]*($|\/.*))/
