7963ff75f74e5fda29d79d40538df0455b3db5f2
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-01-17 10:11:11 +0100

    Cleanup salary menu.

--- lib/bookyt_salary/navigation.rb
+++ lib/bookyt_salary/navigation.rb
@@ -5,10 +5,10 @@ module BookytSalary
                    :if => Proc.new { user_signed_in? } do |salaries|
         salaries.item :salaries, t('bookyt.main_navigation.salaries'), salaries_path, :highlights_on => /\/salaries($|\/[0-9]*($|\/.*))/
         salaries.item :new_salary, t_title(:new, Salary), select_employee_salaries_path
-        salaries.item :employees, t_title(:index, Employee), employees_path
-        salaries.item :employments, t_title(:index, Employment), employments_path, :highlights_on => /\/employments($|\/[0-9]*($|\/.*))/
-        salaries.item :salary_statistics, t_title(:statistics, Salary), statistics_salaries_path, :highlights_on => /\/salaries\/statistics($|\?)/
-        account_item(salaries, '5000')
+        salaries.item :divider, "", :class => 'divider'
+        salaries.item :salary_statistics, t_title(:statistics, Salary), statistics_salaries_path
+        salaries.item :divider, "", :class => 'divider'
+        salaries.item :salary_booking_templates, t_title(:index, SalaryBookingTemplate), salary_booking_templates_path
       end
     end
   end
