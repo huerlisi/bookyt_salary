e83653adce038205d7f6fd9e170bf6ef7acf6eb1
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-01-20 00:07:51 +0100

    Refactor obligation flags to generic saldo inclusion tags.

--- app/helpers/salary_booking_template_helper.rb
+++ app/helpers/salary_booking_template_helper.rb
@@ -1,5 +1,5 @@
 module SalaryBookingTemplateHelper
-  def obligation_collection
-    obligations = SalaryBookingTemplate::OBLIGATION_FLAGS.map{|obligation| [t_attr(obligation), obligation.to_s]}
+  def saldo_inclusion_flag_collection
+    SalaryBookingTemplate.saldo_inclusion_flags.map{|flag| [t_attr('saldo_inclusion_flags.' + flag), flag]}
   end
 end
--- app/models/salary_booking_template.rb
+++ app/models/salary_booking_template.rb
@@ -2,20 +2,8 @@ class SalaryBookingTemplate < BookingTemplate
   default_scope order(:code)
 
   # Obligation flags
-  OBLIGATION_FLAGS = [:for_gross_income, :for_ahv, :for_uvg, :for_uvgz, :for_ktg, :for_deduction_at_source]
-  def obligation=(value)
-    OBLIGATION_FLAGS.each do |flag|
-      self[flag] = value.include?(flag.to_s)
-    end
-  end
-
-  def obligation
-    result = []
-    OBLIGATION_FLAGS.each do |flag|
-      result << flag.to_s if self[flag]
-    end
-
-    result
+  def self.saldo_inclusion_flags
+    ['gross_income', 'ahv', 'uvg', 'uvgz', 'ktg', 'deduction_at_source']
   end
 
   def to_s
