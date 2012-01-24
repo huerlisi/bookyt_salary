9eee598ae3089e71defab6afc6ebb289ff5d55b5
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-01-24 21:57:54 +0100

    Move payslip prawn to prawn/payslip_document.

--- lib/bookyt_salary/railtie.rb
+++ lib/bookyt_salary/railtie.rb
@@ -4,7 +4,5 @@ require 'rails'
 module BookytSalary
   class Railtie < Rails::Engine
     engine_name "bookyt_salary"
-
-    config.autoload_paths << File.expand_path("../lib", __FILE__)
   end
 end
