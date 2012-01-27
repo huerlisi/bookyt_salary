e3549bdb1531b88f2c92637fec1d5881d1a56b8f
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-01-27 17:00:39 +0100

    Add deducation_at_source to SalaryBookingTemplate saldo_inclusion_flags.

--- app/models/salary_booking_template.rb
+++ app/models/salary_booking_template.rb
@@ -1,7 +1,7 @@
 class SalaryBookingTemplate < BookingTemplate
   # Obligation flags
   def self.saldo_inclusion_flags
-    ['gross_income', 'ahv', 'uvg', 'uvgz', 'ktg', 'deduction_at_source']
+    ['gross_income', 'net_income', 'ahv', 'uvg', 'uvgz', 'ktg', 'deduction_at_source']
   end
 
   def to_s
