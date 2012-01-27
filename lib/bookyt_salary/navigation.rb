4138fe16d8591177e90155ba93e6228a27559156
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-01-27 18:36:51 +0100

    Fix salary navigation translations.

--- lib/bookyt_salary/navigation.rb
+++ lib/bookyt_salary/navigation.rb
@@ -1,9 +1,9 @@
 module BookytSalary
   module Navigation
     def setup_bookyt_salary(navigation)
-      navigation.item :salaries, t('bookyt.main_navigation.salaries'), salaries_path,
+      navigation.item :salaries, t_title(:index, Salary), salaries_path,
                    :if => Proc.new { user_signed_in? } do |salaries|
-        salaries.item :salaries, t('bookyt.main_navigation.salaries'), salaries_path, :highlights_on => /\/salaries($|\/[0-9]*($|\/.*))/
+        salaries.item :salaries, t_title(:index, Salary), salaries_path, :highlights_on => /\/salaries($|\/[0-9]*($|\/.*))/
         salaries.item :new_salary, t_title(:new, Salary), select_employee_salaries_path
         salaries.item :divider, "", :class => 'divider'
         salaries.item :salary_statistics, t_title(:statistics, Salary), statistics_salaries_path
