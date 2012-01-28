5661b216245117d27f0a7ee0dde05a67b5d27c83
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-01-28 15:22:01 +0100

    Include line_items instead of direct_bookings in payslip.

--- app/prawn/payslip_document.rb
+++ app/prawn/payslip_document.rb
@@ -1,14 +1,30 @@
 class PayslipDocument < LetterDocument
-  def salary_table(direct_balance, direct_account, direct_bookings)
-    content = direct_bookings.inject([]) do |out, item|
-      title = item.title if item
-      out << [title, nil, nil, nil, currency_fmt(item.accounted_amount(direct_account))] if item.contra_account(direct_account)
-
-      out
+  def salary_table(salary)
+    head = [
+      t_attr(:code, LineItem),
+      t_attr(:title, LineItem),
+      t_attr(:price, LineItem),
+      t_attr(:times, LineItem),
+      t_attr(:accounted_amount, LineItem)
+    ]
+    content = salary.line_items.collect do |item|
+      [item.code, item.title, currency_fmt(item.price), item.times, currency_fmt(item.accounted_amount)]
     end
 
-    rows = content + [total_row(currency_fmt(direct_balance))]
+    rows = [head] + content
 
-    items_table(rows)
+    table(rows, :width => bounds.width) do
+      # General cell styling
+      cells.valign  = :top
+      cells.borders = []
+      cells.padding_bottom = 2
+      cells.padding_top = 2
+
+      # Columns
+      columns(2..4).align = :right
+
+      # Footer styling
+      row(-1).font_style = :bold
+    end
   end
 end
--- app/views/salaries/payslip.pdf.prawn
+++ app/views/salaries/payslip.pdf.prawn
@@ -1,9 +1,6 @@
 prawn_document(:filename => "#{resource.to_s}.pdf", :renderer => PayslipDocument) do |pdf|
   employer = resource.employer
   employee = resource.employee
-  direct_account = Account.find_by_code("5000")
-  direct_bookings = Booking.by_reference(resource)
-  direct_balance = direct_bookings.empty? ? 0.0 : direct_bookings.direct_balance(direct_account)
   
   # Letter header
   pdf.letter_header(employer, employee, resource.to_s)
@@ -12,7 +9,7 @@ prawn_document(:filename => "#{resource.to_s}.pdf", :renderer => PayslipDocument
   pdf.free_text "#{t_attr(:social_security_nr, Employee)}: #{employee.social_security_nr}"
   
   # Line Items
-  pdf.salary_table(direct_balance, direct_account, direct_bookings)
+  pdf.salary_table(resource)
   
   # Closing
   pdf.common_closing(employer) 
