7d3073927aafbc082db9780e791816a84f339b5d
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-01-29 22:57:40 +0100

    Fix translation of saldo_inclusion_flags; use select field for amount_relates_to.

--- app/helpers/salary_booking_template_helper.rb
+++ app/helpers/salary_booking_template_helper.rb
@@ -1,5 +1,12 @@
 module SalaryBookingTemplateHelper
   def saldo_inclusion_flag_collection
-    SalaryBookingTemplate.saldo_inclusion_flags.map{|flag| [t_attr('saldo_inclusion_flags.' + flag), flag]}
+    SalaryBookingTemplate.saldo_inclusion_flags.map{|flag| [t('activerecord.attributes.salary_booking_template.saldo_inclusion_flags.' + flag), flag]}
+  end
+
+  def amount_relates_to_collection
+    saldo_inclusion_flags = SalaryBookingTemplate.saldo_inclusion_flags.map{|flag| [t('activerecord.attributes.salary_booking_template.saldo_inclusion_flags.' + flag), flag]}
+    booking_templates = SalaryBookingTemplate.all.map{|template| [template.title, template.id]}
+
+    return saldo_inclusion_flags + booking_templates
   end
 end
