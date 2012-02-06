f37647c5f23175f0c8ac158b6ce285b7460d5a92
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-02-06 13:19:35 +0100

    Better Title for salary booking template statistics.

--- lib/bookyt_salary/navigation.rb
+++ lib/bookyt_salary/navigation.rb
@@ -6,7 +6,7 @@ module BookytSalary
         salaries.item :salaries, t_title(:index, Salary), salaries_path, :highlights_on => /\/salaries($|\/[0-9]*($|\/.*))/
         salaries.item :new_salary, t_title(:new, Salary), select_employee_salaries_path
         salaries.item :divider, "", :class => 'divider'
-        salaries.item :salary_booking_template_statistics, t_title(:statistics, Salary), statistics_salary_booking_templates_path
+        salaries.item :salary_booking_template_statistics, t_title(:statistics, SalaryBookingTemplate), statistics_salary_booking_templates_path
         salaries.item :divider, "", :class => 'divider'
         salaries.item :salary_booking_templates, t_title(:index, SalaryBookingTemplate), salary_booking_templates_path
         salaries.item :salary_templates, t_title(:index, SalaryTemplate), salary_templates_path
