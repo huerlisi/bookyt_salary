8612f74409dcc86b799b1f6ff2d38bbf15abf4db
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-03-16 12:58:08 +0100

    Fix id for salary list navigation item.

--- lib/bookyt_salary/navigation.rb
+++ lib/bookyt_salary/navigation.rb
@@ -3,7 +3,7 @@ module BookytSalary
     def setup_bookyt_salary(navigation)
       navigation.item :salaries, t_title(:index, Salary), salaries_path,
                    :if => Proc.new { user_signed_in? } do |salaries|
-        salaries.item :salaries, t_title(:index, Salary), salaries_path, :highlights_on => /\/salaries($|\/[0-9]*($|\/.*))/
+        salaries.item :salary_list, t_title(:index, Salary), salaries_path, :highlights_on => /\/salaries($|\/[0-9]*($|\/.*))/
         salaries.item :new_salary, t_title(:new, Salary), select_employee_salaries_path
         salaries.item :divider, "", :class => 'divider'
         salaries.item :salary_booking_template_statistics, t_title(:statistics, SalaryBookingTemplate), statistics_salary_booking_templates_path
