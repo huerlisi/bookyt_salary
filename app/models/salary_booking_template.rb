d63b278c8471c89a058d9ada9caab11d25e4eba3
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-03-20 11:01:12 +0100

    Use map instead of pluck as it gives non-deterministic behaviour.

--- app/models/salary_booking_template.rb
+++ app/models/salary_booking_template.rb
@@ -1,7 +1,8 @@
 class SalaryBookingTemplate < BookingTemplate
   # Obligation flags
   def self.saldo_inclusion_flags
-    SalaryBookingTemplate.tag_counts_on(:include_in_saldo).pluck(:name)
+    # Fix: Using .pluck(:name) would be nice but gives non-deterministic behaviour (id instead of name, sometimes)
+    SalaryBookingTemplate.tag_counts_on(:include_in_saldo).map{|tag| tag.name}
   end
 
   def to_s
