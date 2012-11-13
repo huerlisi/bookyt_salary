ac13203400e4b080b132a2ce793d6a8940540869
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-11-13 09:34:56 +0100

    Include Employee.salaries association definition in BookytSalary::Employee.

--- app/models/bookyt_salary/employee.rb
+++ app/models/bookyt_salary/employee.rb
@@ -3,6 +3,9 @@ module BookytSalary
     extend ActiveSupport::Concern
 
     included do
+      # Associations
+      has_many :salaries, :foreign_key => :company_id
+
       has_many :salary_templates, :foreign_key => :person_id
     end
   end
