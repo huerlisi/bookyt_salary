086e2f414b31bfc2fc29ad02887f2628f9c1af9d
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2014-12-02 22:30:33 +0100

    Drop bogus action title header comment.

--- app/controllers/salary_reports_controller.rb
+++ app/controllers/salary_reports_controller.rb
@@ -1,8 +1,6 @@
 class SalaryReportsController < ApplicationController
   respond_to :html, :pdf
-  
-  # Actions
-  # =======
+
   def yearly_ahv_statement
     @value_period = Date.parse(params[:by_value_period][:from] || Date.today.beginning_of_year)..Date.parse(params[:by_value_period][:to] || Date.today.end_of_year) if params[:by_value_period]
     @value_period ||= Date.today.beginning_of_year..Date.today.end_of_year
