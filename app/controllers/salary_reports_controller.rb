76554172ff68b90c251714f7daa49b7c1103a7e9
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2014-12-02 22:35:55 +0100

    Move common logic in salary reports controller into before filter.

--- app/controllers/salary_reports_controller.rb
+++ app/controllers/salary_reports_controller.rb
@@ -1,17 +1,16 @@
 class SalaryReportsController < ApplicationController
   respond_to :html, :pdf
 
-  def yearly_ahv_statement
+  before_filter do
     @value_period = Date.parse(params[:by_value_period][:from] || Date.today.beginning_of_year)..Date.parse(params[:by_value_period][:to] || Date.today.end_of_year) if params[:by_value_period]
     @value_period ||= Date.today.beginning_of_year..Date.today.end_of_year
+  end
 
+  def yearly_ahv_statement
     @employments = Employment.valid_during(@value_period).all
   end
 
   def salary_declaration
-    @value_period = Date.parse(params[:by_value_period][:from] || Date.today.beginning_of_year)..Date.parse(params[:by_value_period][:to] || Date.today.end_of_year) if params[:by_value_period]
-    @value_period ||= Date.today.beginning_of_year..Date.today.end_of_year
-
     @employee = Employee.find(params[:employee_id])
     @salaries = @employee.salaries.where(:value_date => @value_period)
     @line_items = @salaries.collect{|salary| salary.line_items}.flatten
